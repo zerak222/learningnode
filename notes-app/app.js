391d6c3e6a67c1318c90fe5df87edb7537bdea56
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2020-12-21 22:09:58 +0530

    Added chalk module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,15 @@
-const fs = require('fs');
-fs.writeFileSync('notes.txt','This is Rakesh and this file is created using node.js');
-fs.appendFileSync('notes.txt','.This is the appended line using appendFileSync');
+
+const chalk = require('chalk');
+console.log(chalk.bgRed.inverse.bold('Success:'));
+
+// validator example
+//const validator =  require('validator');
+//console.log(validator.isURL('https://www.google.com/'));
+
+const getNotes = require('./notes.js');
+console.log(getNotes());
+
+
+// const fs = require('fs');
+// fs.writeFileSync('notes.txt','This is Rakesh and this file is created using node.js');
+// fs.appendFileSync('notes.txt','.This is the appended line using appendFileSync');
