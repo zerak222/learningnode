2cd3a6ea5b14e09b931fd6562a733ce534b7f957
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2020-12-22 20:39:35 +0530

    Adding yargs 1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,24 @@
 
-const chalk = require('chalk');
-console.log(chalk.red.cyanBright('Success:'));
+// command line arguments-----------------------------------------Yargs
+// Command : node app.js Name --FirstName="Rakesh" --LastName="Nagapuri"
+const yargs = require('yargs');
+//console.log(process.argv[2]);
+yargs.version('1.0.1');
+console.log(yargs.argv);
 
-// validator example
+// chalk example (decorating text) -------------------------------Chalk
+//const chalk = require('chalk');
+//console.log(chalk.red.cyanBright('Success:'));
+
+// validator example   -------------------------------------------Validator
 //const validator =  require('validator');
 //console.log(validator.isURL('https://www.google.com/'));
 
-const getNotes = require('./notes.js');
-console.log(getNotes());
+// Custom package ------------------------------------------------Custom package
+//const getNotes = require('./notes.js');
+//console.log(getNotes());
 
-
+// Basic file operations------------------------------------------Basic file operations
 // const fs = require('fs');
 // fs.writeFileSync('notes.txt','This is Rakesh and this file is created using node.js');
 // fs.appendFileSync('notes.txt','.This is the appended line using appendFileSync');
