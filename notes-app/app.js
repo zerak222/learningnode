448ab1d5ce7110ab3c89cf7d2f77d9e991c0abed
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2020-12-23 20:57:10 +0530

    yargs 3 : adding options

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,46 @@
 
 // command line arguments-----------------------------------------Yargs
 // Command : node app.js Name --FirstName="Rakesh" --LastName="Nagapuri"
-//const yargs = require('yargs');
-//console.log(process.argv[2]);
-//yargs.version('1.0.1');
-//console.log(yargs.argv);
+const yargs = require('yargs');
+yargs.version('1.0.1');
+// Command : node app.js add --title="Developer" --body="Develops app"
+yargs.command({
+    command : 'add',
+    describe: 'Add a new note',
+    builder : {
+        title : {
+            describe : 'Note Title',
+            demandOption : true,
+            type : 'string'
+        },
+        body :{
+            describe : 'Note body',
+            demandOption : true,
+            type : 'string'
+        }
+    }, 
+    handler : function(argv){
+        console.log('Title : ',argv.title);
+        console.log('Body : ',argv.body);
+    }
+});
+yargs.command({
+    command : 'remove',
+    describe: 'Remove a new note',    
+    handler : function(){
+        console.log('Removing a new note')
+    }
+});
+yargs.command({
+    command : 'list',
+    describe: 'List a new note',    
+    handler : function(){
+        console.log('Listing a new note')
+    }
+});
 
-const myYargs = require('./custompackages/myYargs.js');
+yargs.parse();
+
 
 // chalk example (decorating text) -------------------------------Chalk
 //const chalk = require('chalk');
